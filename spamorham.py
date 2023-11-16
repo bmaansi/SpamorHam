923fbbd1c082fa6efa6753d19dea0183f701fe7d
Ger Thor <gthor2@mail.csuchico.edu>
2023-11-15 17:15:26 -0800

    added extra files that followed the tutorial on the tensorflow documentation

--- spamorham.py
+++ spamorham.py
@@ -30,14 +30,37 @@ y = spambase.data.targets
 # df = pd.DataFrame(y,
 #                     columns = spambase.names)
 
+
+
 # test = pd.DataFrame
 
 # print(df.to_markdown())
-# print(df)
-label = "Class"
+print(y)
+print(X)
 
-train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(y, label=label)
+# Split the dataset into a training and a testing dataset.
 
+label = "word_freq_make"
+
+classes = X[label].unique().tolist()
+print(f"Label classes: {classes}")
+
+X[label] = X[label].map(classes.index)
+
+
+def split_dataset(dataset, test_ratio=0.30):
+  """Splits a panda dataframe in two."""
+  test_indices = np.random.rand(len(dataset)) < test_ratio
+  return dataset[~test_indices], dataset[test_indices]
+
+
+train_ds_pd, test_ds_pd = split_dataset(X)
+print("{} examples in training, {} examples for testing.".format(
+    len(train_ds_pd), len(test_ds_pd)))
+
+
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X, label=label)
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_ds_pd, label=label)
 # %set_cell_height 300
 
 # Specify the model.
@@ -45,3 +68,12 @@ model_1 = tfdf.keras.RandomForestModel(verbose=2)
 
 # Train the model.
 model_1.fit(train_ds)
+
+# print("Found TensorFlow Decision Forests v" + tf.__version__)
+
+model_1.compile(metrics=["accuracy"])
+evaluation = model_1.evaluate(test_ds, return_dict=True)
+print()
+
+for name, value in evaluation.items():
+  print(f"{name}: {value:.4f}")
---
+++ test.py
@@ -0,0 +1,99 @@
+import tensorflow_decision_forests as tfdf
+
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import math
+
+from IPython.display import display
+
+# Download the dataset
+# wget -q https://storage.googleapis.com/download.tensorflow.org/data/palmer_penguins/penguins.csv -O /tmp/penguins.csv
+
+# Load a dataset into a Pandas Dataframe.
+dataset_df = pd.read_csv("spambase.csv")
+
+# Display the first 3 examples.
+dataset_df.head(3)
+
+# Encode the categorical labels as integers.
+#
+# Details:
+# This stage is necessary if your classification label is represented as a
+# string since Keras expects integer classification labels.
+# When using `pd_dataframe_to_tf_dataset` (see below), this step can be skipped.
+
+# Name of the label column.
+label = "A"
+
+classes = dataset_df[label].unique().tolist()
+print(f"Label classes: {classes}")
+
+dataset_df[label] = dataset_df[label].map(classes.index)
+
+# Split the dataset into a training and a testing dataset.
+
+def split_dataset(dataset, test_ratio=0.30):
+  """Splits a panda dataframe in two."""
+  test_indices = np.random.rand(len(dataset)) < test_ratio
+  return dataset[~test_indices], dataset[test_indices]
+
+
+train_ds_pd, test_ds_pd = split_dataset(dataset_df)
+print("{} examples in training, {} examples for testing.".format(
+    len(train_ds_pd), len(test_ds_pd)))
+
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds_pd, label=label)
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_ds_pd, label=label)
+
+# %set_cell_height 300
+
+# Specify the model.
+model_1 = tfdf.keras.RandomForestModel(verbose=2)
+
+# Train the model.
+model_1.fit(train_ds)
+
+model_1.compile(metrics=["accuracy"])
+evaluation = model_1.evaluate(test_ds, return_dict=True)
+print()
+
+for name, value in evaluation.items():
+  print(f"{name}: {value:.4f}")
+
+model_1.save("/tmp/my_saved_model")
+
+tfdf.model_plotter.plot_model_in_colab(model_1, tree_idx=0, max_depth=3)
+
+# %set_cell_height 300
+model_1.summary()
+
+# The input features
+model_1.make_inspector().features()
+
+# The feature importances
+model_1.make_inspector().variable_importances()
+
+model_1.make_inspector().evaluation()
+
+# %set_cell_height 150
+model_1.make_inspector().training_logs()
+
+import matplotlib.pyplot as plt
+
+logs = model_1.make_inspector().training_logs()
+
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot([log.num_trees for log in logs], [log.evaluation.accuracy for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Accuracy (out-of-bag)")
+
+plt.subplot(1, 2, 2)
+plt.plot([log.num_trees for log in logs], [log.evaluation.loss for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Logloss (out-of-bag)")
+
+plt.show()
