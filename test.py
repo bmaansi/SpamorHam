8c61ef30f8f774ce5c9e9a9be94f0141c5750cf5
JesusXPS <jalvarado28@mail.csuchico.edu>
2023-11-27 18:19:33 -0500

    Added 25 Features

--- test.py
+++ test.py
@@ -77,8 +77,8 @@ model_1.make_inspector().variable_importances()
 
 model_1.make_inspector().evaluation()
 
-# %set_cell_height 150
-# model_1.make_inspector().training_logs()
+#%set_cell_height 150
+model_1.make_inspector().training_logs()
 
 # import matplotlib.pyplot as plt
 
@@ -97,3 +97,65 @@ model_1.make_inspector().evaluation()
 # plt.ylabel("Logloss (out-of-bag)")
 
 # plt.show()
+
+tfdf.keras.get_all_models()
+
+# help works anywhere.
+#help(tfdf.keras.RandomForestModel)
+
+# ? only works in ipython or notebooks, it usually opens on a separate panel.
+tfdf.keras.RandomForestModel
+
+feature_1 = tfdf.keras.FeatureUsage(name="word_freq_make")
+feature_2 = tfdf.keras.FeatureUsage(name="word_freq_address")
+feature_3 = tfdf.keras.FeatureUsage(name="word_freq_all")
+feature_4 = tfdf.keras.FeatureUsage(name="word_freq_our")
+feature_5 = tfdf.keras.FeatureUsage(name="word_freq_over")
+feature_6 = tfdf.keras.FeatureUsage(name="word_freq_remove")
+feature_7 = tfdf.keras.FeatureUsage(name="word_freq_internet")
+feature_8 = tfdf.keras.FeatureUsage(name="word_freq_order")
+feature_9 = tfdf.keras.FeatureUsage(name="word_freq_mail")
+feature_10 = tfdf.keras.FeatureUsage(name="word_freq_receive")
+feature_11 = tfdf.keras.FeatureUsage(name="word_freq_will")
+feature_12 = tfdf.keras.FeatureUsage(name="word_freq_people")
+feature_13 = tfdf.keras.FeatureUsage(name="word_freq_report")
+feature_14 = tfdf.keras.FeatureUsage(name="word_freq_addresses")
+feature_15 = tfdf.keras.FeatureUsage(name="word_freq_free")
+feature_16 = tfdf.keras.FeatureUsage(name="word_freq_business")
+feature_17 = tfdf.keras.FeatureUsage(name="word_freq_email")
+feature_18 = tfdf.keras.FeatureUsage(name="word_freq_you")
+feature_19 = tfdf.keras.FeatureUsage(name="word_freq_credit")
+feature_20 = tfdf.keras.FeatureUsage(name="word_freq_your")
+feature_21 = tfdf.keras.FeatureUsage(name="word_freq_font")
+feature_22 = tfdf.keras.FeatureUsage(name="word_freq_000")
+feature_23 = tfdf.keras.FeatureUsage(name="word_freq_money")
+feature_24 = tfdf.keras.FeatureUsage(name="word_freq_hp")
+feature_25 = tfdf.keras.FeatureUsage(name="word_freq_hpl")
+feature_26 = tfdf.keras.FeatureUsage(name="word_freq_george")
+feature_27 = tfdf.keras.FeatureUsage(name="word_freq_650")
+
+
+
+all_features = [feature_1, feature_2, feature_3, feature_4]
+
+# Note: This model is only trained with two features. It will not be as good as
+# the one trained on all features.
+
+model_2 = tfdf.keras.GradientBoostedTreesModel(
+    features=all_features, exclude_non_specified_features=True)
+
+model_2.compile(metrics=["accuracy"])
+model_2.fit(train_ds, validation_data=test_ds)
+
+print(model_2.evaluate(test_ds, return_dict=True))
+
+
+# feature_1 = tfdf.keras.FeatureUsage(name="word_freq_all", semantic=tfdf.keras.FeatureSemantic.CATEGORICAL)
+# feature_2 = tfdf.keras.FeatureUsage(name="word_freq_3d")
+# feature_3 = tfdf.keras.FeatureUsage(name="word_freq_our")
+# all_features = [feature_1, feature_2, feature_3]
+
+# model_3 = tfdf.keras.GradientBoostedTreesModel(features=all_features, exclude_non_specified_features=True)
+# model_3.compile( metrics=["accuracy"])
+
+# model_3.fit(train_ds, validation_data=test_ds)
